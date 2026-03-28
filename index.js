a94a9fae826ac57e5d8d714cd089cc93ee58791f
Mariya Jiji <mariyajiji19082004@gmail.com>
2026-03-28 17:52:14 +0530

    update changes

--- index.js
+++ index.js
@@ -20,17 +20,16 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/providers', require('./routes/providers'));
 app.use('/api/bookings', require('./routes/bookings'));
 app.use('/api/admin', require('./routes/admin'));
-app.use('/api/tracking', require('./routes/tracking'));
 
+
+app.use(cors({
+  origin: "https://your-frontend.vercel.app",
+  credentials: true
+}));
 const complaintsRoutes = require('./routes/complaints');
 app.use('/api/complaints', complaintsRoutes);
-app.get("/", (req, res) => {
-  res.send("HomeEase backend is running 🚀");
-});
+
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
-//MONGO_URI=mongodb+srv://mariyajiji004_db_user:Mariya9745@cluster0.bexbonu.mongodb.net/?appName=Cluster0 
-//JWT_SECRET=homeease_secret_key_2024 
-//PORT=5000
